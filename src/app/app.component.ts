d9e426761bcfd81f3acec43e9928350aff9178c9
iampratiktandel <iampratiktandel@gmail.com>
2022-04-23 22:41:17 +0530

    feat: policy and service filtering on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,15 +17,29 @@ export class AppComponent {
 
     @ViewChild('myChart', { static: true })
     myChart!: ElementRef;
+    public spendByPolicyChartBind!: Chart;
 
     public dataArray: number[];
     public graphData!: any;
+    public graphLegends!: any;
     public monthData!: any;
     public datasetArray!: any;
+    public policyFilterBox: any;
+    public serviceFilterBox!: any;
+    public serviceFilterBoxDisplay!: any;
+    public monthFilterBox!: any;
+    public filteredPolicy!: any;
+    public filteredService!: any;
 
     constructor() {
         this.dataArray = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
         this.datasetArray = [];
+        this.policyFilterBox = [];
+        this.serviceFilterBox = [];
+        this.serviceFilterBoxDisplay = [];
+        this.filteredPolicy = [];
+        this.filteredService = [];
+        this.graphLegends = [];
         // this.graphData = MillionData;
         // this.graphData = DollarData;
         this.graphData = UniData;
@@ -35,14 +49,15 @@ export class AppComponent {
         const d = this.graphData;
         this.monthData = [d.january, d.february, d.march, d.april, d.may, d.june, d.july, d.august, d.september, d.october, d.november, d.december];
         // this.monthData = [ {...this.graphData} ];
-        console.log(this.monthData);
+        // console.log(this.monthData);
+
 
         // console.log(this.datasetArray);
 
         this.generateDataset(this.monthData);
         if (this.myChart) {
             // this.drawBarCanvas();
-            this.drawStackBarCanvas();
+            this.drawStackBarCanvas(this.datasetArray);
         }
     }
 
@@ -50,6 +65,11 @@ export class AppComponent {
         monthData.forEach((monthElement: any, monthIndex: number) => {
             monthElement.forEach((policyElement: any, policyIndex: number) => {
                 let colorIndex = policyIndex === 10 ? 0 : policyIndex;
+                this.generateGraphLegends(policyElement.policy);
+
+                // if (this.graphLegends.find((x: any) => x.policy !== policyElement.policy)) {
+                //     this.generateGraphLegends(policyElement.policy);
+                // }
                 for (const key in policyElement) {
                     if (Object.prototype.hasOwnProperty.call(policyElement, key)) {
                         let lastPushedService = '';
@@ -57,6 +77,7 @@ export class AppComponent {
                             const element = policyElement['serviceData'];
                             element.forEach((element: any) => {
                                 if (element.service !== lastPushedService) {
+                                    this.generateServiceFilterBox(element.service);
                                     const filter = this.datasetArray?.find(({ label }: any) => label === `${element.service} [${policyElement.policy}]`);
                                     if (filter) {
                                         this.datasetArray.forEach((elem: any) => {
@@ -67,32 +88,42 @@ export class AppComponent {
                                     } else {
                                         let dummyArray = [null, null, null, null, null, null, null, null, null, null, null, null];
                                         dummyArray.splice(monthIndex, 0, element.amount);
+                                        let backgroundColor = this.setColor(colorIndex, monthIndex);
                                         this.datasetArray.push({
                                             label: `${element.service} [${policyElement.policy}]`,
                                             data: dummyArray,
-                                            backgroundColor: [
-                                                this.setColor(colorIndex, monthIndex)
-                                            ],
+                                            backgroundColor,
                                             stack: `Stack ${policyIndex}`
                                         })
-                                        this.datasetArray
+                                        this.updateGraphLegend(policyIndex, element.service, backgroundColor, dummyArray);
                                     }
                                     lastPushedService = element.service;
                                 }
                             });
                         }
+
                     }
                 }
+
+                this.generatePolicyFilterBox(policyElement.policy);
             })
         });
-        console.log(this.datasetArray);
+        // console.log(this.policyFilterBox);
+        // console.log(this.serviceFilterBox);
+        // console.log(this.datasetArray);
+        // console.log(this.graphLegends);
     }
 
-    drawStackBarCanvas() {
+
+    drawStackBarCanvas(datasetArray: any) {
         const canvas = this.myChart.nativeElement;
         const ctx = canvas.getContext('2d');
 
-        const myChart = new Chart(ctx, {
+        if (this.spendByPolicyChartBind) {
+            this.spendByPolicyChartBind.destroy();
+        }
+
+        this.spendByPolicyChartBind = new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: [
@@ -109,7 +140,7 @@ export class AppComponent {
                     'November',
                     'December'
                 ],
-                datasets: this.datasetArray,
+                datasets: datasetArray,
                 // datasets: [
                 //   {
                 //     label: 'Policy 1 - Service 1',
@@ -156,49 +187,130 @@ export class AppComponent {
                 // ]
             },
             options: {
-                plugins: {
-                    title: {
-                        display: true,
-                        text: 'Chart.js Bar Chart - Stacked'
-                    },
-                    //   tooltip: {
-                    //     callbacks: {
-                    //       label: function (context: any) {
-                    //         // context.dataset.hoverBackgroundColor = '#000';
-                    //         // context.dataset.backgroundColor = '#000';
-                    //         // context.chart.config._config.data.datasets[2].backgroundColor = '#000';
-                    //         let value = context.parsed.y.toFixed(1);
-                    //         return '$' + value;
-                    //       },
-                    //     },
-                    //   }
-                },
                 responsive: true,
-                interaction: {
-                    intersect: false,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: true
+                    },
+                    tooltip: {
+                        callbacks: {
+                            label: function (context: any) {
+                                return context.dataset.label + ':' + '$' + context.formattedValue;
+                            },
+                        },
+                    },
                 },
                 scales: {
                     x: {
                         stacked: true,
+                        title: {
+                            display: true,
+                            text: 'MONTH',
+                        },
                     },
                     y: {
-                        beginAtZero: true,
-                        ticks: {
-                            count: 100
+                        stacked: true,
+                        title: {
+                            display: true,
+                            text: 'DOLLARS',
                         },
-                        stacked: true
                     }
                 }
-            },
+            }
             // plugins: [ChartDataLabels]
         })
     }
 
 
+    filterData() {
+        let result = [];
+        if (this.filteredPolicy.length && !this.filteredService.length) {
+            result = this.datasetArray.filter((d: any) => {
+                return this.filteredPolicy.includes(d.label.split('[')['1'].replace(']', ''));
+            });
+        } else if (!this.filteredPolicy.length && this.filteredService.length) {
+            result = this.datasetArray.filter((d: any) => {
+                return this.filteredService.includes(d.label.split('[')['0'].trim());
+            });
+        } else if (this.filteredPolicy.length && this.filteredService.length) {
+            let subResult = this.datasetArray.filter((d: any) => {
+                return this.filteredPolicy.includes(d.label.split('[')['1'].replace(']', ''));
+            });
+            result = subResult.filter((d: any) => {
+                return this.filteredService.includes(d.label.split('[')['0'].trim());
+            });
+            // console.log(subResult);
+        } else {
+            result = this.datasetArray;
+        }
+        // console.log(result);
+        this.drawStackBarCanvas(result);
+    }
 
+    generatePolicyFilterBox(policyName: string) {
+        let isNotUnique = this.policyFilterBox.find((x: any) => x.policy === policyName);
+        if (!isNotUnique) {
+            this.policyFilterBox.push(
+                {
+                    policy: policyName,
+                    isChecked: false
+                }
+            )
+        }
+    }
 
+    generateServiceFilterBox(serviceName: string) {
+        let isNotUnique = this.serviceFilterBox.find((x: any) => x.service === serviceName);
+        if (!isNotUnique) {
+            this.serviceFilterBox.push(
+                {
+                    service: serviceName,
+                    isChecked: false
+                }
+            )
+        }
+        this.serviceFilterBoxDisplay = this.serviceFilterBox;
+    }
 
+    generateGraphLegends(policy: string) {
+        this.graphLegends.push({
+            policy: policy,
+            serviceData: []
+        })
+        console.log(this.graphLegends);
+    }
 
+    updateGraphLegend(policyIndex: number, service: number, backgroundColor: string, data: any) {
+        this.graphLegends[policyIndex].serviceData.push({
+            service: service,
+            backgroundColor: backgroundColor,
+            data: data
+        })
+        // console.log(this.graphLegends);
+    }
+
+    onPolicyClick(item: any) {
+        this.filteredPolicy = [];
+        item.isChecked = !item.isChecked;
+        this.policyFilterBox.forEach((elem: any) => {
+            if (elem.isChecked) {
+                this.filteredPolicy.push(elem.policy);
+            }
+        })
+        this.filterData();
+    }
+
+    onServiceClick(item: any) {
+        this.filteredService = [];
+        item.isChecked = !item.isChecked;
+        this.serviceFilterBox.forEach((elem: any) => {
+            if (elem.isChecked) {
+                this.filteredService.push(elem.service);
+            }
+        })
+        this.filterData();
+    }
 
 
 
